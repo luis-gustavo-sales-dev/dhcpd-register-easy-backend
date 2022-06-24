e2e2ba0529085db50abb59d4d37d116a878cd41f
Luis Gustavo Sales Dev <luisgssf@gmail.com>
2022-06-24 15:50:37 -0300

    Atualizacao de Registro de Dispositivo funcionando corretamente.

--- DhcpRegister/src/main/java/br/dev/luisgustavosales/dhcpregister/controller/DeviceRegisterController.java
+++ DhcpRegister/src/main/java/br/dev/luisgustavosales/dhcpregister/controller/DeviceRegisterController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,6 +63,16 @@ public class DeviceRegisterController {
 		var deviceRegisterCreated = this.deviceRegisterService.create(deviceRegister);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(deviceRegisterCreated);
+		
+	}
+	
+	@PutMapping("/{cpf}/{mac}")
+	public ResponseEntity<DeviceRegister> update(
+			@PathVariable String cpf,
+			@PathVariable String mac,
+			@RequestBody DeviceRegister deviceRegister) {
+		var dr = this.deviceRegisterService.update(cpf, mac, deviceRegister);
+		return ResponseEntity.ok(dr);
 		
 	}
 	
--- DhcpRegister/src/main/java/br/dev/luisgustavosales/dhcpregister/services/DeviceRegisterService.java
+++ DhcpRegister/src/main/java/br/dev/luisgustavosales/dhcpregister/services/DeviceRegisterService.java
@@ -21,27 +21,6 @@ public class DeviceRegisterService {
 	
 	@Autowired
 	private DeviceUserGroupRepository deviceUserGroupRepository;
-	
-	public DeviceRegister create(DeviceRegister deviceRegister) {
-		var cpf = deviceRegister.getIds().getCpf();
-		var mac = deviceRegister.getIds().getMac();
-		
-		var deviceRegisterAlreadyExists = deviceRegisterRepository.findByIdsCpfAndIdsMac(cpf, mac);
-		
-		deviceRegisterAlreadyExists.ifPresent( s -> { 
-				throw new CpfAndMacAlreadyExistsException("Este registro com cpf " + cpf +
-						" e mac " + mac + " já existe!");
-			});
-		
-		// Precisa verificar se o grupo é válido antes de criar
-		
-		deviceUserGroupRepository.findById(deviceRegister.getGroup().getId())
-			.orElseThrow( 
-					() -> new DeviceUserGroupNotFoundException("Não há nenhum " +
-							"grupo associado a esse id: " + deviceRegister.getGroup().getId()));
-		
-		return this.deviceRegisterRepository.save(deviceRegister);
-	}
 
 	public DeviceRegister findByCpfAndMac(String cpf, String mac) {
 		
@@ -67,6 +46,63 @@ public class DeviceRegisterService {
 			return null;
 		}
 		return dr.get();
+	}
+	
+	public DeviceRegister create(DeviceRegister deviceRegister) {
+		var cpf = deviceRegister.getIds().getCpf();
+		var mac = deviceRegister.getIds().getMac();
+		
+		var deviceRegisterAlreadyExists = deviceRegisterRepository.findByIdsCpfAndIdsMac(cpf, mac);
+		
+		deviceRegisterAlreadyExists.ifPresent( s -> { 
+				throw new CpfAndMacAlreadyExistsException("Este registro com cpf " + cpf +
+						" e mac " + mac + " já existe!");
+			});
+		
+		// Precisa verificar se o grupo é válido antes de criar
+		
+		deviceUserGroupRepository.findById(deviceRegister.getGroup().getId())
+			.orElseThrow( 
+					() -> new DeviceUserGroupNotFoundException("Não há nenhum " +
+							"grupo associado a esse id: " + deviceRegister.getGroup().getId()));
+		
+		return this.deviceRegisterRepository.save(deviceRegister);
+	}
+	
+	public DeviceRegister update(String cpf, String mac, DeviceRegister deviceRegisterToUpdate) {
+		
+		// Existe um registro com o mac e cpf informados?
+		var dr = deviceRegisterRepository.findByIdsCpfAndIdsMac(cpf, mac)
+				.orElseThrow( 
+						() -> new CpfAndMacNotFoundException(
+								"Não há registro de dispositivo com Cpf " + 
+										cpf + " e Mac " + mac + "!."));
+		
+		// O grupo informado é válido?
+		deviceUserGroupRepository.findById(deviceRegisterToUpdate.getGroup().getId())
+			.orElseThrow( 
+					() -> new DeviceUserGroupNotFoundException("Não há nenhum " +
+							"grupo associado a esse id: " + deviceRegisterToUpdate.getGroup().getId()));
+		
+		// Existe outro registro com o CPF e MAC informados para atualização
+		deviceRegisterRepository.findByIdsCpfAndIdsMac(
+				deviceRegisterToUpdate.getIds().getCpf(), 
+				deviceRegisterToUpdate.getIds().getMac())
+					.ifPresent( 
+							(d) ->  {
+									throw new CpfAndMacAlreadyExistsException(
+										"Já existe um registro com o Cpf " + 
+												deviceRegisterToUpdate.getIds().getCpf() + " e Mac " + 
+												deviceRegisterToUpdate.getIds().getMac() + 
+												" informados para atualização.");
+								});
+		
+		// Apague o registro anterior e crie um novo
+		this.deviceRegisterRepository.delete(dr);
+		
+		// Cria um novo registro com os dados informados.
+		return this.deviceRegisterRepository.save(deviceRegisterToUpdate);
+		
 	}
 	
 	public void deleteDeviceRegisterByCpfAndMac(String cpf, String mac) {
